662ecdd5398da5972c89241b9d80d51345464a64
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-05-05 23:53:32 +0500

    feat:  add user type

--- src/shared/types/user.type.ts
+++ src/shared/types/user.type.ts
@@ -1,6 +1,7 @@
 export type User = {
+  name: string;
   email: string;
-  avatarPath: string;
-  firstname: string;
-  lastname: string;
-}
+  avatarPath?: string;
+  password: string;
+  userType: 'ordinary' | 'pro';
+};
