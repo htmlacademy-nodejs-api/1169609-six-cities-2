285723daf0e1fa9ecd803c64053042e8c5b5304e
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-05-06 01:31:03 +0500

    feat: add comment type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,6 +1,7 @@
 export * from './user.type.js';
 export * from './user-type.enum.js';
 export * from './offer.type.js';
+export * from './comment.type.js';
 export * from './location.type.js';
 export * from './offer-images.type.js';
 export * from './city-type.enum.js';
