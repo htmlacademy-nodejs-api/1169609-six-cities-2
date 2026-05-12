f4ae8f9e1f3a537a4c0477277648b8f39624dc14
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-05-13 00:49:30 +0500

    feat: dynamic command import

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
-import { CLIApplication, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import { CLIApplication } from './cli/index.js';
+import { glob } from 'glob';
+import { Command } from './cli/commands/command.interface.js';
+import { resolve } from 'node:path';
+import { pathToFileURL } from 'node:url';
 
-function bootstrap() {
+async function bootstrap() {
   const cliApplication = new CLIApplication();
-  cliApplication.registerCommands([
-    new HelpCommand(),
-    new VersionCommand(),
-    new ImportCommand(),
-  ]);
+
+  const importedCommands: Command[] = [];
+  const files = glob.sync('src/cli/commands/*.command.ts'); //TODO вынести путь в конфиг
+
+  for (const file of files) {
+    const modulePath = resolve(file);
+    const moduleExportItems = await import(pathToFileURL(modulePath).href);
+
+    for (const exportKey of Object.keys(moduleExportItems)) {
+      const exportItem = moduleExportItems[exportKey];
+      if (exportItem.prototype && typeof exportItem.prototype.execute === 'function') {
+        const commandInstance = new exportItem();
+        importedCommands.push(commandInstance);
+      }
+    }
+
+    // if (typeof CommandClass === 'function') {
+    //   const commandInstance = new CommandClass();
+    //
+    // }
+  }
+
+  cliApplication.registerCommands(importedCommands);
 
   cliApplication.processCommand(process.argv);
 }
