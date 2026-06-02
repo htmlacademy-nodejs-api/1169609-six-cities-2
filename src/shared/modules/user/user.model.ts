cbcc6e60e7cb58a7bf0cfac49bac6f1c063e6ba9
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-06-03 01:20:12 +0500

    4.11. WIP feat: add User model

---
+++ src/shared/modules/user/user.model.ts
@@ -0,0 +1,37 @@
+import { Schema, Document, model } from 'mongoose';
+import { User, UserRole } from '../../types/index.js';
+
+export interface UserDocument extends User, Document {}
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  avatarPath: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userRole: {
+    type: String,
+    required: true,
+    enum: Object.values(UserRole),
+  },
+},
+{
+  timestamps: true
+}
+);
+
+export const UserModel = model<UserDocument>('User', userSchema);
