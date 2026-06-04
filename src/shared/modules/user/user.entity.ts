081916ffc87d7828cd0855e9b531fc4d2c12fc58
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-06-05 00:06:47 +0500

    4.17. WIP feat: add model and schema definition

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserRole } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true, minlength: [1, 'Min length for firstname is 1'] })
   public name: string;
+
+  @prop({ required: true, unique: true })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarPath?: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true, enum: UserRole })
   public userRole: UserRole;
 }
+
+export const UserModel = getModelForClass(UserEntity);
