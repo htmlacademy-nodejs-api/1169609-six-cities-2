39e917d5a58fcee05a5096a965114ce97b65e6a0
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-06-05 20:32:38 +0500

    4.20. WIP feat: add methods to UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,12 +1,19 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { User, UserRole } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  @prop({ required: true, minlength: [1, 'Min length for firstname is 1'] })
+  @prop({ required: true, default: '' })
   public name: string;
 
   @prop({ required: true, unique: true })
@@ -15,11 +22,28 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, default: '' })
   public avatarPath?: string;
 
-  @prop({ required: true })
-  public password: string;
+  @prop({ required: true, default: '' })
+  private password?: string; //TODO 
 
   @prop({ required: true, enum: UserRole })
   public userRole: UserRole;
+
+  constructor(userData: User) {
+    super();
+
+    this.name = userData.name;
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.userRole = userData.userRole;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
